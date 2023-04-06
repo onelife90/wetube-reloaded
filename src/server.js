409f43e4e2aa00d61a2871961e41d116087c694f
onelife90 <onelife90@naver.com>
2023-04-06 17:55:15 +0900

    Added: Middleware

--- src/server.js
+++ src/server.js
@@ -3,9 +3,31 @@ import express from "express";
 const PORT = 4000;
 const app = express();
 
+const logger = (req, res, next) => {
+  console.log(`${req.method} ${req.url}`);
+  next();
+};
+
+const privateMiddlewater = (req, res, next) => {
+  const url = req.url;
+  if (url === "/protected") {
+    return res.send("<h1>Not Allowed</h1>");
+  }
+  console.log("Allow, you may continue.");
+  next();
+};
+
+const handleProtected = (req, res) => {
+  return res.send("Welcome to the private lounge.");
+};
+
+app.use(logger);
+app.use(privateMiddlewater);
+
 app.get("/", (req, res) => {
-  return res.send("<h1>I still powerful!</h1>");
+  return res.send("I love middlewares");
 });
+app.get("/protected", handleProtected);
 
 app.get("/login", (req, res) => {
   return res.send({ message: "Login here." });
