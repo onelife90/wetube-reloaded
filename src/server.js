94f4a0ac287c0aa304cd4fe47467854b8976b245
onelife90 <onelife90@naver.com>
2023-04-06 18:56:42 +0900

    Added: morgan middleware

--- src/server.js
+++ src/server.js
@@ -1,33 +1,14 @@
 import express from "express";
+import morgan from "morgan";
 
 const PORT = 4000;
 const app = express();
-
-const logger = (req, res, next) => {
-  console.log(`${req.method} ${req.url}`);
-  next();
-};
-
-const privateMiddlewater = (req, res, next) => {
-  const url = req.url;
-  if (url === "/protected") {
-    return res.send("<h1>Not Allowed</h1>");
-  }
-  console.log("Allow, you may continue.");
-  next();
-};
-
-const handleProtected = (req, res) => {
-  return res.send("Welcome to the private lounge.");
-};
+const logger = morgan("dev");
 
 app.use(logger);
-app.use(privateMiddlewater);
-
 app.get("/", (req, res) => {
   return res.send("I love middlewares");
 });
-app.get("/protected", handleProtected);
 
 app.get("/login", (req, res) => {
   return res.send({ message: "Login here." });
