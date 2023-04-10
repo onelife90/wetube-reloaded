0432c8e5e512210f7030542956a2278fb1dcc09c
onelife90 <onelife90@naver.com>
2023-04-10 15:14:22 +0900

    Added: edit viedo using Relative URL

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -3,23 +3,23 @@ let videos = [
     title: "inhale video",
     rating: 5,
     comments: 2,
-    createdAt: "2 minutes agd",
-    views: 59,
+    createdAt: "2 minutes ago",
+    views: 1,
     id: 1,
   },
   {
     title: "exhale video",
     rating: 5,
     comments: 2,
-    createdAt: "2 minutes agd",
-    views: 59,
+    createdAt: "2 minutes ago",
+    views: 0,
     id: 2,
   },
   {
     title: "breath video",
     rating: 5,
     comments: 2,
-    createdAt: "2 minutes agd",
+    createdAt: "2 minutes ago",
     views: 59,
     id: 3,
   },
@@ -28,10 +28,10 @@ let videos = [
 export const trending = (req, res) => {
   return res.render("home", { pageTitle: "Home", videos });
 };
-export const see = (req, res) => {
+export const watch = (req, res) => {
   const { id } = req.params;
   const video = videos[id - 1];
-  return res.render("watch", { pageTitle: `Watching ${video.title}` });
+  return res.render("watch", { pageTitle: `Watching ${video.title}`, video });
 };
 export const edit = (req, res) => res.render("edit", { pageTitle: "Edit" });
 export const search = (req, res) => res.send("Search Videos");
