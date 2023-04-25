8a3b7920cec5d1d4160fddaadc5afdf28b5c212a
onelife90 <onelife90@naver.com>
2023-04-25 13:46:58 +0900

    Added: connect mongoDB and mongoose

--- src/server.js
+++ src/server.js
@@ -1,10 +1,11 @@
+import "./db";
 import express from "express";
 import morgan from "morgan";
 import globalRouter from "./routers/globalRouter";
 import videoRouter from "./routers/videoRouter";
 import userRouter from "./routers/userRouter";
 
-const PORT = 4000;
+const PORT = 8000;
 
 const app = express();
 const logger = morgan("dev");
@@ -19,6 +20,6 @@ app.use("/videos", videoRouter);
 app.use("/users", userRouter);
 
 const handleListening = () =>
-  console.log(`Server listening on port http://localhost:${PORT}`);
+  console.log(`✅ Server listening on http://localhost:${PORT}`);
 
 app.listen(PORT, handleListening);
