30e188dd1041ff07cf040287e5c8b8dee12eb88c
onelife90 <onelife90@naver.com>
2023-04-25 19:27:32 +0900

    Modified: editVideo template

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -5,25 +5,37 @@ export const home = async (req, res) => {
   console.log(videos);
   return res.render("home", { pageTitle: "Home", videos });
 };
+
 export const watch = async (req, res) => {
   const { id } = req.params;
   const video = await Video.findById(id);
-  console.log(video);
+  if (!video) {
+    return res.render("404", { pageTitle: "Video not found." });
+  }
   return res.render("watch", { pageTitle: video.title, video });
 };
-export const getEdit = (req, res) => {
+
+export const getEdit = async (req, res) => {
   const { id } = req.params;
-  return res.render("edit", { pageTitle: `Editing` });
+  const video = await Video.findById(id);
+  if (!video) {
+    return res.render("404", { pageTitle: "Video not found." });
+  }
+  return res.render("edit", { pageTitle: `Edit:${video.title}`, video });
 };
+
 export const postEdit = (req, res) => {
   const { id } = req.params;
   const { title } = req.body;
   return res.redirect(`/videos/${id}`);
 };
+
 export const search = (req, res) => res.send("Search Videos");
+
 export const getUpload = (req, res) => {
   return res.render("upload", { pageTitle: "Upload video" });
 };
+
 export const postUpload = async (req, res) => {
   const { title, description, hastags } = req.body;
   try {
@@ -40,4 +52,5 @@ export const postUpload = async (req, res) => {
     });
   }
 };
+
 export const deleteVideo = (req, res) => res.send("Delete Videos");
