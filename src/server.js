0bce13fd5b142ae3a92cd3fc499a15a84e6a75e8
onelife90 <onelife90@naver.com>
2023-04-28 18:36:59 +0900

    Added: login user using session & res.locals

--- src/server.js
+++ src/server.js
@@ -1,5 +1,7 @@
 import express from "express";
 import morgan from "morgan";
+import session from "express-session";
+import { localsMiddleware } from "./middlewares";
 import globalRouter from "./routers/rootRouter";
 import videoRouter from "./routers/videoRouter";
 import userRouter from "./routers/userRouter";
@@ -12,6 +14,15 @@ app.set("views", process.cwd() + "/src/views");
 
 app.use(logger);
 app.use(express.urlencoded({ extended: true }));
+app.use(
+  session({
+    secret: "Wetube!",
+    resave: true,
+    saveUninitialized: true,
+  })
+);
+app.use(localsMiddleware);
+
 app.use("/", globalRouter);
 app.use("/videos", videoRouter);
 app.use("/users", userRouter);
