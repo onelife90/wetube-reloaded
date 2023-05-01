79203debd8602b3fc4be22995000833dda33f3a3
onelife90 <onelife90@naver.com>
2023-05-01 20:14:06 +0900

    MAdded: hidden change-password to socialOnly user

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -185,6 +185,9 @@ export const postEdit = async (req, res) => {
   return res.redirect("/users/edit");
 };
 export const getChangePassword = (req, res) => {
+  if (req.session.user.socialOnly === true) {
+    return res.redirect("/");
+  }
   return res.render("users/change-password", { pageTitle: "Change Password" });
 };
 export const postChangePassword = (req, res) => {
