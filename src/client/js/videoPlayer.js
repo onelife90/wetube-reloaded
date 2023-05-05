694e0a17f561a36adda0d1ec6a9b81d206eb016e
onelife90 <onelife90@naver.com>
2023-05-05 09:23:21 +0900

    Added: video volume mute & unmute

--- src/client/js/videoPlayer.js
+++ src/client/js/videoPlayer.js
@@ -2,22 +2,18 @@ const video = document.querySelector("video");
 const playBtn = document.getElementById("play");
 const muteBtn = document.getElementById("mute");
 const time = document.getElementById("time");
-const volume = document.getElementById("volume");
+const volumeRange = document.getElementById("volume");
 
 const handlePlayClick = (e) => {
-  if (video.paused) {
-    video.play();
-  } else {
-    video.pause();
-  }
+  video.paused ? video.play() : video.pause();
+  playBtn.innerText = video.paused ? "Play" : "Pause";
 };
 
-const handlePause = () => (playBtn.innerText = "Play");
-const handlePlay = () => (playBtn.innerText = "Pause");
-
-const handleMute = (e) => {};
+const handleMute = (e) => {
+  video.muted = video.muted ? false : true;
+  muteBtn.innerText = video.muted ? "Unmute" : "Mute";
+  volumeRange.value = video.muted ? 0 : 0.5;
+};
 
 playBtn.addEventListener("click", handlePlayClick);
 muteBtn.addEventListener("click", handleMute);
-video.addEventListener("pause", handlePause);
-video.addEventListener("play", handlePlay);
