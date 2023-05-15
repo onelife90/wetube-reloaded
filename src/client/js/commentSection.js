48d05b103cf991b27de5586967b05f52cbfe3473
onelife90 <onelife90@naver.com>
2023-05-15 21:44:07 +0900

    Added: apiRouter to commentSection using express-json

--- src/client/js/commentSection.js
+++ src/client/js/commentSection.js
@@ -5,7 +5,15 @@ const handleSubmit = (event) => {
   event.preventDefault();
   const textarea = commentForm.querySelector("textarea");
   const text = textarea.value;
-  const video = videoContainer.dataset.id;
+  const videoId = videoContainer.dataset.id;
+  if (text.trim() === "") {
+    return;
+  }
+  fetch(`/api/videos/${videoId}/comment`, {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ text }),
+  });
 };
 
 if (commentForm) {
