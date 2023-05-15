68b4a55766c1bd9c345d13aa846f63c22115f081
onelife90 <onelife90@naver.com>
2023-05-15 18:48:57 +0900

    FixBug: view commentbox only login state

--- src/client/js/commentSection.js
+++ src/client/js/commentSection.js
@@ -1,12 +1,13 @@
 const videoContainer = document.getElementById("videoContainer");
 const commentForm = document.getElementById("commentForm");
-const textarea = commentForm.querySelector("textarea");
-const button = commentForm.querySelector("button");
 
 const handleSubmit = (event) => {
   event.preventDefault();
+  const textarea = commentForm.querySelector("textarea");
   const text = textarea.value;
   const video = videoContainer.dataset.id;
 };
 
-commentForm.addEventListener("submit", handleSubmit);
+if (commentForm) {
+  commentForm.addEventListener("submit", handleSubmit);
+}
